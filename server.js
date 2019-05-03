a7c2819369a6b1226d7a7a1ad53cd47f6027aab6
Hoang Le <38028938+hoangle4@users.noreply.github.com>
2019-05-03 01:15:44 -0700

    commit backend GoogleOAuth

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
-require("dotenv").config();
-var express = require("express");
-var exphbs = require("express-handlebars");
+require('dotenv').config();
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const passport = require('passport');
+const db = require('./models');
 
-var db = require("./models");
-
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Handlebars
 app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
+	'handlebars',
+	exphbs({
+		defaultLayout: 'main'
+	})
 );
-app.set("view engine", "handlebars");
-
+app.set('view engine', 'handlebars');
+app.use(
+	session({
+		secret: 'secret',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+//Passport Stratergy
+require('./passport/passport')(passport);
+app.use(passport.initialize());
+app.use(passport.session());
+const authRoutes = require('./routes/authRoutes');
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+app.use('/auth', authRoutes);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
-var syncOptions = { force: false };
+let syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+if (process.env.NODE_ENV === 'test') {
+	syncOptions.force = true;
 }
 
 // Starting the server, syncing our models ------------------------------------/
 db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+	app.listen(PORT, function() {
+		console.log('==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT);
+	});
 });
 
 module.exports = app;
