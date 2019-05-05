31fbd43a1e245afb6feebe384ea27765acc46ecf
KMZ <mzlatnik@gmail.com>
2019-05-05 10:09:31 -0700

    added bones for shared_lists_all and shared_lists_single. Added html routes for these pages. Small CSS changes for pages.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,52 @@
-var db = require('../models');
+var db = require("../models");
 
 module.exports = function(app) {
-	// Get all examples
-	app.get('/api/examples', function(req, res) {
-		db.Example.findAll({}).then(function(dbExamples) {
-			res.json(dbExamples);
-		});
-	});
+  // put calls for user's lists here
+  
+  
+  
+  
+  
+  // GET route for getting all of the lists shared by friends
+  app.get("/api/lists/", function(req, res) {
+    db.Lists.findAll({}).then(function(dbLists) {  // this is more in line with getting the user's lists.  Need specs for getting shared lists
+      res.json(dbLists);
+    });
+  });
 
-	// Create a new example
-	app.post('/api/examples', function(req, res) {
-		db.Example.create(req.body).then(function(dbExample) {
-			res.json(dbExample);
-		});
-	});
+  // Get route for returning lists by name
+  app.get("/api/lists/listname/:listname", function(req, res) {
+    db.Lists
+      .findAll({
+        where: {
+          ListName: req.params.listname
+        }
+      })
+      .then(function(dbLists) {
+        res.json(dbLists);
+      });
+  });
 
-	// Delete an example by id
-	app.delete('/api/examples/:id', function(req, res) {
-		db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-			res.json(dbExample);
-		});
-	});
+  // Get all examples
+  /*   app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.json(dbExamples);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/examples", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
+      res.json(dbExample);
+    });
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
+      res.json(dbExample);
+    });
+  }); */
 };
