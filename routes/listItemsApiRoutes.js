4aae4c22eac465498492113d17c09210fd0f5541
Hoang Le <hoang.le4@pcc.edu>
2019-05-09 19:41:34 -0700

    commit adding list item

--- routes/listItemsApiRoutes.js
+++ routes/listItemsApiRoutes.js
@@ -1,32 +1,33 @@
 var db = require('../models');
 
-module.exports = function(app) {
+module.exports = function (app) {
 	/// API's for list Items
 
-	app.get('/api/listItems/:listId', function(req, res) {
+	app.get('/api/listItems/:listId', function (req, res) {
 		db.ListItems
 			.findAll({
 				where: {
 					ListId: req.params.listId
 				}
 			})
-			.then(function(results) {
+			.then(function (results) {
 				// We have access to the todos as an argument inside of the callback function
 				res.render('users/editList', {
-					list: results
+					list: results,
+					listId: req.params.listId
 				});
 			})
 			.catch((err) => console.log(err));
 	});
 
-	app.get('/api/listItems/edit/:id', function(req, res) {
+	app.get('/api/listItems/edit/:id', function (req, res) {
 		db.ListItems
 			.findAll({
 				where: {
 					id: req.params.id
 				}
 			})
-			.then(function(results) {
+			.then(function (results) {
 				// We have access to the todos as an argument inside of the callback function
 				res.render('users/listItemEdit', {
 					list: results[0].dataValues
@@ -36,21 +37,21 @@ module.exports = function(app) {
 	});
 
 	// Create a new example
-	app.post('/api/listItems/:listId', function(req, res) {
+	app.post('/api/listItems/:listId', function (req, res) {
 		db.ListItems
 			.create({
-				title: req.body.title,
+				title: req.body.name,
 				url: req.body.url,
-				imgUrl: req.body.imgUrl,
+				imgUrl: req.body.imgURL,
 				price: req.body.price,
 				notes: req.body.notes,
 				ListId: req.params.listId
 			})
-			.then(function(results) {
+			.then(function (results) {
 				// We have access to the new todo as an argument inside of the callback function
-				res.json(results);
+				res.redirect(`/api/listItems/${req.params.listId}`);
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				// Whenever a validation or flag fails, an error is thrown
 				// We can "catch" the error to prevent it from being "thrown", which could crash our node app
 				res.json(err);
@@ -58,19 +59,19 @@ module.exports = function(app) {
 	});
 
 	// Delete an example by id
-	app.delete('/api/listItems/:id', function(req, res) {
+	app.delete('/api/listItems/:id', function (req, res) {
 		db.ListItems
 			.destroy({
 				where: {
 					id: req.params.id
 				}
 			})
-			.then(function(results) {
+			.then(function (results) {
 				res.json(results);
 			});
 	});
 
-	app.put('/api/listItems/:id/:ListId', function(req, res) {
+	app.put('/api/listItems/:id/:ListId', function (req, res) {
 		const newListItem = {
 			title: req.body.title,
 			url: req.body.url,
@@ -85,11 +86,11 @@ module.exports = function(app) {
 					id: req.params.id
 				}
 			})
-			.then(function(results) {
+			.then(function (results) {
 				console.log(results)
 				res.redirect(`/api/listItems/${req.params.ListId}`);
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				// Whenever a validation or flag fails, an error is thrown
 				// We can "catch" the error to prevent it from being "thrown", which could crash our node app
 				res.json(err);
