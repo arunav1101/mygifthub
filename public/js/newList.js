653c383d5aeb78441e79c3b53d7e033994ef4a12
Arunav Gupta <arunav1101@gmail.com>
2019-05-10 13:45:29 -0700

    fix delete functionality

--- public/js/newList.js
+++ public/js/newList.js
@@ -13,26 +13,6 @@ $(document).ready(function () {
 
   // Adding an event listener for when the form is submitted
   $(newListForm).on("submit", handleFormSubmit);
-  // $("#userShared").on("submit", shareFormSubmit);
-
-
-  // Gets the part of the url that comes after the "?" (which we have if we're updating a list)
-  //var url = window.location.search;
-  //var listId;
-
-  // Sets a flag for whether or not we're updating a list to be false initially
-  //var updating = false;
-
-  // If we have this section in our url, we pull out the list id from the url
-  // In '?list_id=1', listId is 1
- /*  if (url.indexOf("?list_id=") !== -1) {
-    listId = url.split("=")[1];
-    getListData(listId, "list");
-  } */
-
-  // A function for handling what happens when the form to create a new list is submitted
-
-  // var userId;
 
   // Submits a new list and brings user to launch page upon completion
   function submitList(list) {
@@ -46,41 +26,6 @@ $(document).ready(function () {
         window.location.href = "/dashboard";
       }
     );
-
-
-    //   jQuery.post(`/api/list/${userId}`, list, function() {
-    //   window.location.href = "/dashboard";
-    // });
-  }
-
-  // Gets list data for the current list if we're editing
- /*  function getListData(id, type) {
-    var queryUrl = "/api/list/" + id;
-    $.ajax(queryUrl, {
-      type: "GET"
-    }).then(function (data) {
-      if (data) {
-        console.log(data.ListName || data.id);
-        // If this list exists, prefill our cms forms with its data
-        nameInput.val(data.ListName);
-        category = data.category;
-        // If we have a list with this id, set a flag for us to know to update the list
-        // when we hit submit
-        updating = true;
-      }
-    });
-  } */
-
-  // Update a given list, bring user to the dashboard page when done
-/*   function updateList(list) {
-    $.ajax({
-      method: "PUT",
-      url: "/api/lists",
-      data: list
-    }).then(function () {
-      window.location.href = "/dashboard";
-    });
-  } */
 
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -101,15 +46,8 @@ $(document).ready(function () {
         Category: categorySelect.val(),
         GoogleID: googleInput.val()
       };
-
-      // If we're updating a list run updateList to update a list
-      // Otherwise run submitList to create a whole new list
-/*       if (updating) {
-        newList.id = listId;
-        updateList(newList);
-      } else { */
         submitList(newList);
-      //}
+
     });
   }
   $(".sharedBtn").on("click", function (event) {
@@ -145,7 +83,14 @@ $.ajax("/api/users/email/" + $("#sharedEmailId").val(), {
   });
 });
 
-    //     });
-    // });
-    // });
+$(".deleteBtn").on("click", function (event) {
+  dataStore.listId = event.target.dataset.listid;
+  console.log('delete',dataStore.listId);
+  $.ajax("/api/list/" + dataStore.listId, {
+    type: "delete",
+  }).then(function (response) {
+    console.log(response);
 });
+});
+
+ });
