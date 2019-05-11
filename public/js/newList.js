c474da8ce22c563cc9994774e451f69f8e79225b
Arunav Gupta <arunav1101@gmail.com>
2019-05-10 18:04:53 -0700

    fix bug of redirecting the page to dashboard after delete

--- public/js/newList.js
+++ public/js/newList.js
@@ -26,71 +26,69 @@ $(document).ready(function () {
         window.location.href = "/dashboard";
       }
     );
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    // Wont submit the list if we are missing a name, or category
-    if (!nameInput.val().trim() || !categorySelect.val()) {
-      return;
     }
-    var myvalue = googleInput.val();
+    function handleFormSubmit(event) {
+      event.preventDefault();
+      // Wont submit the list if we are missing a name, or category
+      if (!nameInput.val().trim() || !categorySelect.val()) {
+        return;
+      }
+      var myvalue = googleInput.val();
 
-    $.ajax("/api/users/id/" + myvalue, {
-      type: "GET"
-    }).then(function (res) {
-      dataStore.userId = res.id;
-      console.log('userID', dataStore.userId);
-      // Constructing a newList object to hand to the database
-      var newList = {
-        ListName: nameInput.val().trim(),
-        Category: categorySelect.val(),
-        GoogleID: googleInput.val()
-      };
+      $.ajax("/api/users/id/" + myvalue, {
+        type: "GET"
+      }).then(function (res) {
+        dataStore.userId = res.id;
+        console.log('userID', dataStore.userId);
+        // Constructing a newList object to hand to the database
+        var newList = {
+          ListName: nameInput.val().trim(),
+          Category: categorySelect.val(),
+          GoogleID: googleInput.val()
+        };
         submitList(newList);
 
-    });
-  }
-  $(".sharedBtn").on("click", function (event) {
-   dataStore.listId = event.target.dataset.listid;
-    $('.modal-body').empty();
-    $("#Mymodal").modal("show");
+      });
+    }
+    $(".sharedBtn").on("click", function (event) {
+      dataStore.listId = event.target.dataset.listid;
+      $('.modal-body').empty();
+      $("#Mymodal").modal("show");
 
-    $('.modal-body').append(`<div class="form-group">
+      $('.modal-body').append(`<div class="form-group">
   <label for="usr">Enter the Email Address of your friend</label>
   <input type="text" class="form-control" id="sharedEmailId">
 </div>`);
-  });
+    });
 
-$(".sharedModal").on("click", function (event) {
-  event.preventDefault();
-  console.log($("#sharedEmailId").val());
-$.ajax("/api/users/email/" + $("#sharedEmailId").val(), {
-  type: "GET"
-}).then(function (res) {
+    $(".sharedModal").on("click", function (event) {
+      event.preventDefault();
+      console.log($("#sharedEmailId").val());
+      $.ajax("/api/users/email/" + $("#sharedEmailId").val(), {
+        type: "GET"
+      }).then(function (res) {
 
-  console.log('sharedModall respose', res);
-      console.log('UserId', res.id);
-      var sharedToUser = {
-        sharedTo: res.id
-      };
+        console.log('sharedModall respose', res);
+        console.log('UserId', res.id);
+        var sharedToUser = {
+          sharedTo: res.id
+        };
 
-      $.ajax("/api/shared/" + dataStore.listId, {
-        type: "post",
-        data: sharedToUser
+        $.ajax("/api/shared/" + dataStore.listId, {
+          type: "post",
+          data: sharedToUser
+        }).then(function (response) {
+          console.log(response);
+        });
+      });
+    });
+
+    $(".deleteBtn").on("click", function (event) {
+      dataStore.listId = event.target.dataset.listid;
+      $.ajax("/api/list/" + dataStore.listId, {
+        type: "delete",
       }).then(function (response) {
         console.log(response);
+      });
     });
-  });
-});
-
-$(".deleteBtn").on("click", function (event) {
-  dataStore.listId = event.target.dataset.listid;
-  console.log('delete',dataStore.listId);
-  $.ajax("/api/list/" + dataStore.listId, {
-    type: "delete",
-  }).then(function (response) {
-    console.log(response);
-});
-});
-
- });
+})
