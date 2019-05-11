b74140b6e407fde5e0a56e31fbf8d952262766ce
Hoang Le <38028938+hoangle4@users.noreply.github.com>
2019-05-11 01:32:21 -0700

    commit client side

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const methodOverride = require("method-override");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const { shareListName } = require("./config/helpers");
-
 // Middleware
 app.use(
   express.urlencoded({
